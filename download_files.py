43a10b46aa7e99ab09e7e4f9a998970cc9f92838
Rejekts <101389292+RejektsAI@users.noreply.github.com>
2024-09-27 17:46:18 -0500

    Update download_files.py

--- download_files.py
+++ download_files.py
@@ -4,12 +4,16 @@ if not os.path.exists(assets_folder):
     os.makedirs(assets_folder)
 files = {
     "rmvpe/rmvpe.pt":"https://huggingface.co/Rejekts/project/resolve/main/rmvpe.pt",
-    "hubert/hubert_base.pt":"https://huggingface.co/Rejekts/project/resolve/main/hubert_base.pt"
+    "hubert/hubert_base.pt":"https://huggingface.co/Rejekts/project/resolve/main/hubert_base.pt",
+    "f0D32k.pth":"https://huggingface.co/lj1995/%s%s%s/resolve/main/pretrained_v2/%s" % ("Voice","Conversion","WebUI","f0D32k.pth","f0D32k.pth"),
+    "f0G32k.pth":"https://huggingface.co/lj1995/%s%s%s/resolve/main/pretrained_v2/%s" % ("Voice","Conversion","WebUI","f0G32k.pth","f0G32k.pth"),
+    "f0Ov2Super32kD.pth":"https://huggingface.co/lj1995/%s%s%s/resolve/main/pretrained_v2/%s" % ("Voice","Conversion","WebUI","f0Ov2Super32kD.pth","f0Ov2Super32kD.pth"),
+    "f0Ov2Super32kG.pth":"https://huggingface.co/lj1995/%s%s%s/resolve/main/pretrained_v2/%s" % ("Voice","Conversion","WebUI","f0Ov2Super32kG.pth","f0Ov2Super32kG.pth"),
 }
 for file, link in files.items():
     file_path = os.path.join(assets_folder, file)
     if not os.path.exists(file_path):
         try:
-            subprocess.run(['wget', link, '-O', file_path], check=True)
+            subprocess.run(['wget', '-nc', link, '-O', file_path], check=True)
         except subprocess.CalledProcessError as e:
             print(f"Error downloading {file}: {e}")
