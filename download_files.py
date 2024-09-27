fdb40c82e92ce6e9cf7885e5e926749d50988ecc
Rejekts <101389292+RejektsAI@users.noreply.github.com>
2024-09-27 18:06:24 -0500

    Update download_files.py

--- download_files.py
+++ download_files.py
@@ -5,10 +5,10 @@ if not os.path.exists(assets_folder):
 files = {
     "rmvpe/rmvpe.pt":"https://huggingface.co/Rejekts/project/resolve/main/rmvpe.pt",
     "hubert/hubert_base.pt":"https://huggingface.co/Rejekts/project/resolve/main/hubert_base.pt",
-    "f0D32k.pth":"https://huggingface.co/Rejekts/project/resolve/main/f0D32k.pth",
-    "f0G32k.pth":"https://huggingface.co/Rejekts/project/resolve/main/f0G32k.pth",
-    "f0Ov2Super32kD.pth":"https://huggingface.co/Rejekts/project/resolve/main/f0Ov2Super32kD.pth",
-    "f0Ov2Super32kG.pth":"https://huggingface.co/Rejekts/project/resolve/main/f0Ov2Super32kG.pth",
+    "pretrained_v2/f0D32k.pth":"https://huggingface.co/Rejekts/project/resolve/main/f0D32k.pth",
+    "pretrained_v2/f0G32k.pth":"https://huggingface.co/Rejekts/project/resolve/main/f0G32k.pth",
+    "pretrained_v2/f0Ov2Super32kD.pth":"https://huggingface.co/Rejekts/project/resolve/main/f0Ov2Super32kD.pth",
+    "pretrained_v2/f0Ov2Super32kG.pth":"https://huggingface.co/Rejekts/project/resolve/main/f0Ov2Super32kG.pth",
 }
 for file, link in files.items():
     file_path = os.path.join(assets_folder, file)
