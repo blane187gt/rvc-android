618e27ad6766f0d65200df78eb6388cd1003c673
Rejekts <101389292+RejektsAI@users.noreply.github.com>
2024-09-30 12:00:28 -0500

    Update download_files.py

--- download_files.py
+++ download_files.py
@@ -9,6 +9,10 @@ files = {
     "pretrained_v2/f0G32k.pth":"https://huggingface.co/Rejekts/project/resolve/main/f0G32k.pth",
     "pretrained_v2/f0Ov2Super32kD.pth":"https://huggingface.co/Rejekts/project/resolve/main/f0Ov2Super32kD.pth",
     "pretrained_v2/f0Ov2Super32kG.pth":"https://huggingface.co/Rejekts/project/resolve/main/f0Ov2Super32kG.pth",
+    "audios/someguy.mp3":"https://huggingface.co/Rejekts/project/resolve/main/someguy.mp3",
+    "audios/somegirl.mp3:"https://huggingface.co/Rejekts/project/resolve/main/somegirl.mp3",
+    "audios/unchico.mp3:"https://huggingface.co/Rejekts/project/resolve/main/unchico.mp3",
+    "audios/unachica.mp3:"https://huggingface.co/Rejekts/project/resolve/main/unachica.mp3"
 }
 for file, link in files.items():
     file_path = os.path.join(assets_folder, file)
