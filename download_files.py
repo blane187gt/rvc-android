8599e02ce94f39a45de2d30a1eb0b9ac90e31d1b
Rejekts <101389292+RejektsAI@users.noreply.github.com>
2024-09-30 15:35:21 -0500

    Update download_files.py

--- download_files.py
+++ download_files.py
@@ -12,7 +12,8 @@ files = {
     "audios/someguy.mp3": "https://huggingface.co/Rejekts/project/resolve/main/someguy.mp3",
     "audios/somegirl.mp3": "https://huggingface.co/Rejekts/project/resolve/main/somegirl.mp3",
     "audios/unchico.mp3": "https://huggingface.co/Rejekts/project/resolve/main/unchico.mp3",
-    "audios/unachica.mp3": "https://huggingface.co/Rejekts/project/resolve/main/unachica.mp3"
+    "audios/unachica.mp3": "https://huggingface.co/Rejekts/project/resolve/main/unachica.mp3",
+    "a.png": "https://huggingface.co/Rejekts/project/resolve/main/a.png"
 }
 for file, link in files.items():
     file_path = os.path.join(cwd, file)
