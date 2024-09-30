cf56d4a0bd422403171876a7f93bdad93cfe2a65
Rejekts <101389292+RejektsAI@users.noreply.github.com>
2024-09-30 12:01:43 -0500

    Update download_files.py

--- download_files.py
+++ download_files.py
@@ -3,16 +3,16 @@ assets_folder = "./assets/"
 if not os.path.exists(assets_folder):
     os.makedirs(assets_folder)
 files = {
-    "rmvpe/rmvpe.pt":"https://huggingface.co/Rejekts/project/resolve/main/rmvpe.pt",
-    "hubert/hubert_base.pt":"https://huggingface.co/Rejekts/project/resolve/main/hubert_base.pt",
-    "pretrained_v2/f0D32k.pth":"https://huggingface.co/Rejekts/project/resolve/main/f0D32k.pth",
-    "pretrained_v2/f0G32k.pth":"https://huggingface.co/Rejekts/project/resolve/main/f0G32k.pth",
-    "pretrained_v2/f0Ov2Super32kD.pth":"https://huggingface.co/Rejekts/project/resolve/main/f0Ov2Super32kD.pth",
-    "pretrained_v2/f0Ov2Super32kG.pth":"https://huggingface.co/Rejekts/project/resolve/main/f0Ov2Super32kG.pth",
-    "audios/someguy.mp3":"https://huggingface.co/Rejekts/project/resolve/main/someguy.mp3",
-    "audios/somegirl.mp3:"https://huggingface.co/Rejekts/project/resolve/main/somegirl.mp3",
-    "audios/unchico.mp3:"https://huggingface.co/Rejekts/project/resolve/main/unchico.mp3",
-    "audios/unachica.mp3:"https://huggingface.co/Rejekts/project/resolve/main/unachica.mp3"
+    "rmvpe/rmvpe.pt": "https://huggingface.co/Rejekts/project/resolve/main/rmvpe.pt",
+    "hubert/hubert_base.pt": "https://huggingface.co/Rejekts/project/resolve/main/hubert_base.pt",
+    "pretrained_v2/f0D32k.pth": "https://huggingface.co/Rejekts/project/resolve/main/f0D32k.pth",
+    "pretrained_v2/f0G32k.pth": "https://huggingface.co/Rejekts/project/resolve/main/f0G32k.pth",
+    "pretrained_v2/f0Ov2Super32kD.pth": "https://huggingface.co/Rejekts/project/resolve/main/f0Ov2Super32kD.pth",
+    "pretrained_v2/f0Ov2Super32kG.pth": "https://huggingface.co/Rejekts/project/resolve/main/f0Ov2Super32kG.pth",
+    "audios/someguy.mp3": "https://huggingface.co/Rejekts/project/resolve/main/someguy.mp3",
+    "audios/somegirl.mp3": "https://huggingface.co/Rejekts/project/resolve/main/somegirl.mp3",
+    "audios/unchico.mp3": "https://huggingface.co/Rejekts/project/resolve/main/unchico.mp3",
+    "audios/unachica.mp3": "https://huggingface.co/Rejekts/project/resolve/main/unachica.mp3"
 }
 for file, link in files.items():
     file_path = os.path.join(assets_folder, file)
