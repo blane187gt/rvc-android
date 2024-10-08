3bfa416f1eca041cfca0f2946467e47fc95edab0
Rejekts <101389292+RejektsAI@users.noreply.github.com>
2024-10-07 22:02:34 -0500

    Update download_files.py

--- download_files.py
+++ download_files.py
@@ -1,7 +1,11 @@
 import subprocess, os
-cwd = "."
-if not os.path.exists(cwd):
-    os.makedirs(cwd)
+cwd = os.getcwd()
+
+directories = ["logs", "assets", "outputs", "assets/rmvpe", "assets/hubert", "audios", "assets/rmvpe","assets/weights"]
+
+for directory in directories:
+    os.makedirs(os.path.join(cwd, directory), exist_ok=True)
+
 files = {
     "assets/rmvpe/rmvpe.pt": "https://huggingface.co/Rejekts/project/resolve/main/rmvpe.pt",
     "assets/hubert/hubert_base.pt": "https://huggingface.co/Rejekts/project/resolve/main/hubert_base.pt",
@@ -14,10 +18,11 @@ files = {
     "audios/unchico.mp3": "https://huggingface.co/Rejekts/project/resolve/main/unchico.mp3",
     "audios/unachica.mp3": "https://huggingface.co/Rejekts/project/resolve/main/unachica.mp3"
 }
+
 for file, link in files.items():
     file_path = os.path.join(cwd, file)
     if not os.path.exists(file_path):
         try:
-            subprocess.run(['wget', link, '-O', file_path], check=True)
+            subprocess.run(['wget', '-O', file_path, link], check=True)
         except subprocess.CalledProcessError as e:
             print(f"Error downloading {file}: {e}")
